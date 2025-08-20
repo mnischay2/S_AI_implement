5a2bbcf8f0f58492ff5f2752c8b58f102eb33fd7
mnischay2 <mnischay2@gmail.com>
2025-08-20 16:00:18 +0530

    added create url

--- app/src/upload.py
+++ app/src/upload.py
@@ -3,7 +3,8 @@ import requests
 import csv
 
 # === Configuration ===
-API_URL = "http://bodh_uat.sarthhakai.com:5173/experiments/94dc835e-8551-465c-8c90-f34fdc2dae65"
+API_URL = "http://bodh_uat.sarthhakai.com/utilitiesservice/upload-files/documents"
+create_URL = "http://bodh_uat.sarthhakai.com/userservice/createDocument"
 AUTH_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJjMGJmMGQ0OC04MWY2LTRmYjctYWMwOS02MjU2NjNhYjBlOTIiLCJ0SWQiOiI2YmE2NzA3NC0wNjE1LTQ4OWMtYmMwYy03OTEwYjA0MDYzYzIiLCJvcmdJZCI6ImUxNjhlMWU1LTEyMzEtNGE4ZC1hYmNmLWQ2NmM3NDU1MjIyOCIsImZpcnN0TmFtZSI6IklJUEQiLCJsYXN0TmFtZSI6ImJvZGhpIiwicm9sZSI6IlVTRVIiLCJzdWJzY3JpcHRpb25fcXVhbnRpdHkiOjgsImlhdCI6MTc1MjgzNjIzNiwiZXhwIjo0OTA4NTk2MjM2fQ.rce5Ayeaslhv5ZcjUX5mEc9MbgaVQzAhyOhn_reFDc0"  # Replace with your valid bearer token
 CSV_FOLDER = "csv_data"
 
@@ -22,11 +23,13 @@ def upload_csv(filename):
             files = {'files': (filename, file, 'text/csv')}
             headers = {'Authorization': f'Bearer {AUTH_TOKEN}'}
             response = requests.post(API_URL, files=files, headers=headers)
-
+    
         if response.status_code == 200:
             print(f"✅ Uploaded: {filename}")
             os.remove(filepath)
+            print(response.text)
             return("done")
+        
         else:
             print(f"❌ Failed to upload {filename}: {response.status_code} - {response.text}")
             return (f"❌ Failed to upload {filename}: {response.status_code} - {response.text}")
